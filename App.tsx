c340b7f1d663f75e9c8f17a5679bc9c332d65b85
lucassantosdasilva1 <42879994+lucassantosdasilva1@users.noreply.github.com>
2022-05-27 17:27:34 -0300

    initial app.tsx

--- App.tsx
+++ App.tsx
@@ -1,18 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet} from 'react-native';
-import { Provider } from 'react-redux';
-import { store } from './src/redux';
+import { StyleSheet, Text, View } from 'react-native';
 
-import { Routes } from "./src/routes"
+import {
+  ApplicationProvider,
+  Button,
+  Icon,
+  IconRegistry,
+  Layout,
+} from "@ui-kitten/components";
+
+import * as eva from '@eva-design/eva';
 
 export default function App() {
   return (
-    <Provider store={store}>
-    
-      <Routes/>
-    </Provider>
- 
+    <ApplicationProvider theme={eva.light} {...eva}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ApplicationProvider>
   );
 }
 
